dd31a5ace074aa420058544b436fbbf350e963e6
meepo <homheihdu@gmail.com>
2014-09-08 09:12:27 +0800

    remove index.js from models to app/controller

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function IndexModel(res, req) {
+  res.render('index');
+};
--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var IndexModel = require('../models/index')
+var index      = require('../models/index')
   , bee        = require('apibee')
   , configPath = __dirname + '../config/config.json';
 
 module.exports = function(router) {
   var api = new bee(router, 'v1', configPath)
-    , model = new IndexModel();
+
   api.group('Index', function(api) {
     api.get('/', function(req, res) {
-      res.render('index', model);
+      res.render('index', index.index);
     });
   })
 };
